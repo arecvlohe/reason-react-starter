ac8d6523bc6e57b62ecef9209e677d124cdb72a4
Adam Recvlohe <adam.recvlohe@gmail.com>
2019-06-21 00:37:50 -0500

    Update deps
    - Lock deps
    - Define node version
    - Refactor code

--- src/client.js
+++ src/client.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css";
 
-import App from "../lib/es6_global/src/App";
+import { make as App } from "../lib/es6_global/src/App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
